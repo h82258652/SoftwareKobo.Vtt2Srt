ebc316c248e3e273caf088fbb7beb4cad43b5af2
h82258652 <842053625@qq.com>
2014-12-03 00:27:31 +0800

    finished

---
+++ SoftwareKobo.VttToSrt.Core/SoftwareKobo.VttToSrt.Core/VttToSrt.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftwareKobo.VttToSrt.Core
+{
+    public static class VttToSrt
+    {
+        public static string Convert(string vtt)
+        {
+            if (vtt.StartsWith("WEBVTT") == false)
+            {
+                throw new ArgumentException("not support", "vtt");
+            }
+            // remove "WEBVTT"
+            vtt = vtt.Substring(6);
+
+            var spans = vtt.Split(new[] { "\r\n\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            var srt = new List<string>();
+            var index = 1;
+            for (int i = 0; i < spans.Length; i++)
+            {
+                var span = spans[i];
+                span = span.Trim('\r', '\n');
+                srt.Add(index.ToString());
+
+                var array = span.Split(new[] { "\r\n" }, StringSplitOptions.None);
+                srt.Add(array[0].Trim());
+                var sb = new StringBuilder();
+                for (int j = 1; j < array.Length; j++)
+                {
+                    sb.Append(array[j].Trim());
+                }
+                srt.Add(sb.ToString());
+                srt.Add(Environment.NewLine);
+
+                index++;
+            }
+            var temp = string.Join(Environment.NewLine, srt);
+            return temp.Replace("\r\n\r\n", "\r\n");
+        }
+    }
+}
---
+++ SoftwareKobo.VttToSrt.Core/SoftwareKobo.VttToSrt.Winform/MainForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace SoftwareKobo.VttToSrt.Winform
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            using (var ofd = new OpenFileDialog())
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    var vtt = File.ReadAllText(ofd.FileName);
+                    var srt = Core.VttToSrt.Convert(vtt);
+                    using (var sfd = new SaveFileDialog())
+                    {
+                        if (sfd.ShowDialog() == DialogResult.OK)
+                        {
+                            File.WriteAllText(sfd.FileName, srt);
+                            MessageBox.Show("finish");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
